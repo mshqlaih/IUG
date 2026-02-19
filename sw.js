d83276c1559cca68f1685e919bb8b2d90cf72af5
mshqlaih <60563969+mshqlaih@users.noreply.github.com>
2026-02-19 20:40:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+const CACHE_NAME = 'quran-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './xlsx.full.min.js'
+];
+
+// تثبيت الملفات في الذاكرة
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// تشغيل التطبيق من الذاكرة حتى لو لا يوجد إنترنت
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
