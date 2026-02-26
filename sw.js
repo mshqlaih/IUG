32fc5038e75d6fb0b8359b69c5e57017913a3745
mshqlaih <60563969+mshqlaih@users.noreply.github.com>
2026-02-26 10:02:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quran-app-v1.55';
+const CACHE_NAME = 'quran-app-v1.56';
 const ASSETS = [
   './',
   './index.html',
@@ -36,6 +36,7 @@ self.addEventListener('activate', (e) => {
     })
   );
 });
+
 
 
 
