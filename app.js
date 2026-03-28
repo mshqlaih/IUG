c0ac53806a1b2c8336e01ed393f45b29ef8ebbf2
mshqlaih <60563969+mshqlaih@users.noreply.github.com>
2026-03-28 11:48:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ if ('serviceWorker' in navigator) {
                 }
             };
         };
-
         
         // استقبال تاريخ آخر تحديث من الـ SW
         navigator.serviceWorker.addEventListener('message', (event) => {
