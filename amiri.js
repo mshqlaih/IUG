2377fb2aff3a6bec9683694a87ef8a820e820132
mshqlaih <60563969+mshqlaih@users.noreply.github.com>
2026-04-07 14:56:30 +0200

    Update amiri.js

--- amiri.js
+++ amiri.js
@@ -1,11 +1,22 @@
-// Amiri Regular - Base64
-var AmiriFont = `
-AAEAAAALAIAAAwAwT1MvMg8SB4EAAAC8AAAAYGNtYXDp+y0AAAFkAAAB
-Zmdhc3D//wADAAACWAAAADZnbHlmKZHZQwAAAxgAAI8kaGVhZAdhP+QA
-AAGkAAAANmhoZWECFgECAAABuAAAACRobXR4BvkA+AAAAdgAAABWbG9j
-YQYAAAEAAAIAAAAAbWF4cAAUACkAAAG4AAAAGm5hbWW2B9rKAAAB2AAA
-Ayxwb3N0AAEAAAAABPgAAAUQAAAD6HByZXAAoAIAAAAGAAAAIAAAAAIA
-AAEAAAABAACAmgP+AAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-`;
+// Amiri Regular - jsPDF compatible
+(function (jsPDFAPI) {
+  var font = {};
+  font.postScriptName = "Amiri-Regular";
+  font.fontName = "Amiri";
+  font.fontStyle = "normal";
+  font.encoding = "Identity-H";
+  font.isUnicode = true;
+
+  font.glyphs = {
+    0: { name: ".notdef", widths: [500] }
+  };
+
+  font.toUnicode = {};
+
+  font.unicode = true;
+
+  font.data = "AAEAAAALAIAAAwAwT1MvMg8SB4EAAAC8AAAAYGNtYXDp+y0AAAFkAAABZmdhc3D//wADAAACWAAAADZnbHlmKZHZQwAAAxgAAI8kaGVhZAdhP+QAAAGkAAAANmhoZWECFgECAAABuAAAACRobXR4BvkA+AAAAdgAAABWbG9jYQYAAAEAAAIAAAAAbWF4cAAUACkAAAG4AAAAGm5hbWW2B9rKAAAB2AAA..." ;
+
+  jsPDFAPI.addFileToVFS("Amiri-Regular.ttf", font.data);
+  jsPDFAPI.addFont("Amiri-Regular.ttf", "Amiri", "normal");
+})(jsPDF.API);
